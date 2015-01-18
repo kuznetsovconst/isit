76304b63af2ab8e4d13ee98784282429012a16f9
Konstantin <konstantin@iMac-Konstantin.local>
2015-01-18 21:39:30 +0300

    page about frame done

---
+++ kuznetsov_AI_lab/js/site.js
@@ -0,0 +1,13 @@
+$(function () {
+            $("[data-toggle='tooltip']").tooltip();
+        });
+
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+$("#menu-toggle-2").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
